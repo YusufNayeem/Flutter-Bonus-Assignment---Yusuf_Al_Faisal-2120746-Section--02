cf8e837077e73179c15b14eb7cc15897cbc21ea1
YusufNayeem <2120746@iub.edu.bd>
2026-04-17 23:31:30 +0600

    Bertho Cheshta 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:flutter_ui_class/providers/task_management_provider.dart';
 import 'package:flutter_ui_class/screens/UI_page.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async{
+
+  //await Firebase.initializeApp(
+    //options: DefaultFirebaseOptions.currentPlatform,
+  //);
+
   runApp(const FlutterUIApp());
 }
 
